736862902a8dbb34c34be64f3047a9f4238011d3
karthikshanbhag <karthikshanbhag969@gmail.com>
2022-07-13 21:48:02 +0530

    dxodsahncdujahswvb

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,5 +6,5 @@ const Item = ({todo,key,setTodos}) => {
         <li key={key} >{todo} &nbsp; <button onClick={()=>handleDelete(todo)}>remove</button></li>
      );
 }
- adasdasdasdasd
+<h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis facere, omnis minus quisquam mollitia, nisi placeat inventore in quam, rerum obcaecati tenetur eligendi minima id voluptatem culpa deleniti modi nulla!</h1>
 export default Item;
