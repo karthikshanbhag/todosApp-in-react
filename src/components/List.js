c4d9c9100472dc4e50251678024bae22ec55a85c
karthikshanbhag <karthikshanbhag969@gmail.com>
2022-07-14 22:49:22 +0530

    222

--- src/components/List.js
+++ src/components/List.js
@@ -4,7 +4,7 @@ const  List= ({todos,setTodos}) => {
         <ol>
             {
         //    props.todos.map((todo,index) => <li key={index}>{todo}</li>)
-        todos.le0?"No todos yet": todos.map((todo,i) => <Item setTodos={setTodos} todo={todo} key={i}/>
+        todos.length===0?"No todos yet": todos.map((todo,i) => <Item setTodos={setTodos} todo={todo} key={i}/>
         //  {
         //     return <li key={i}>{todo}</li>
 
