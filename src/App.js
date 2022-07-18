7582f8ecd7fcf25abd738ea3b0c745b6e098604d
karthikshanbhag <karthikshanbhag969@gmail.com>
2022-07-18 20:53:18 +0530

    upp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Heading from "./components/Heading";
 import AddTodo from './components/AddTodo';
 import List from "./components/List";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
+
 const App = () => {
   // let count=0;
-  let [todos,setTodos] =useState([])
+  // let [todos,setTodos] =useState([])
+  cons t [Count,setCount] = useState(0)
+  const [value,setValue] = useState('')
+
+  useEffect(()=>{
+    console.log('effect ran')
+  })
   // const handleIncreament =(e) =>setCount(prev=>prev+1)
   // const handleDecreament =(e) =>setCount(prev=>prev-1)
   // const message = "Hello World";
@@ -12,9 +19,8 @@ const App = () => {
   // const todos = ["task1","task2","task3"];
   return ( 
     <div>
-     <Heading/>
-     <AddTodo setTodos={setTodos}/>
-      <List setTodos={setTodos} todos={todos} />
+      <button onClick={()=>setCount(count=>count+1)}>Increament</button>
+      <input onChange={()=>setValue(prev=>prev+"a")} type="text" />
     </div>
    );
 }
